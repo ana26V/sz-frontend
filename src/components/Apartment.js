a6c8392446e969d3dd06df50711b65f59b7401b2
ana26V <anamariavoichescu26@gmail.com>
2023-12-22 17:48:03 +0200

    ???

--- src/components/Apartment.js
+++ src/components/Apartment.js
@@ -7,7 +7,7 @@ import { Col, Row } from 'react-bootstrap';
 const RoomCard = ({ room }) => {
   return (
     <Card className="text-center mb-3 mx-3">
-      <Card.Img variant="top" height={200} width={200} src={room.image_urls[0]} />
+      <Card.Img variant="top" src={room.image_urls} />
       <Card.Body>
         <Card.Title>Room: {room.room_number}</Card.Title>
         <Card.Text>
@@ -21,18 +21,36 @@ const RoomCard = ({ room }) => {
 
 function Apartment({ apartment, fromDate, toDate }) {
 
-  let user = null;
-  try {
-    const currentUser = localStorage.getItem('currentUser');
-    if (currentUser) {
-      user = JSON.parse(currentUser);
-    }
-  } catch (error) {
-    console.error('Error parsing user data:', error);
-    // Handle the parsing error, such as clearing the localStorage or displaying an error message.
-  }
+    console.log(apartment.current_bookings)
 
-  const isUserLoggedIn = !!user; 
+  // eslint-disable-next-line no-unused-vars
+  const isRoomTaken = () => {
+    //CHANGE HERRE
+    if (!apartment || !apartment.current_bookings || !fromDate || !toDate) {
+      return false;
+    }
+
+    const checkFromDate = new Date(fromDate.split('-').reverse().join('-')); // Reformat fromDate to YYYY-MM-DD
+    const checkToDate = new Date(toDate.split('-').reverse().join('-')); // Reformat toDate to YYYY-MM-DD
+
+    for (const booking of apartment.current_bookings) {
+      const bookingFromDate = new Date(booking.fromDate.split('-').reverse().join('-')); // Reformat booking.fromDate to YYYY-MM-DD
+      const bookingToDate = new Date(booking.toDate.split('-').reverse().join('-')); // Reformat booking.toDate to YYYY-MM-DD
+
+      if (
+        (checkFromDate <= bookingToDate && checkFromDate >= bookingFromDate) ||
+        (checkToDate >= bookingFromDate && checkToDate <= bookingToDate) ||
+        (checkFromDate <= bookingFromDate && checkToDate >= bookingToDate)
+      ) {
+        return true; // Room is taken during the provided date range
+      }
+    }
+
+    return false; // Room is available for the provided date range
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const hasImages = apartment && apartment.roomID.image_urls && apartment.roomID.image_urls.length > 0;
   //const roomAvailable = !isRoomTaken(); // Check if the room is available
   const roomAvailable =true;
   return (
@@ -51,24 +69,18 @@ function Apartment({ apartment, fromDate, toDate }) {
       
     {(isValidDateFormat(fromDate) && isValidDateFormat(toDate)) ? (
                //COME BACK HERE AND CHANGE
-               isUserLoggedIn ? ( // Check if the user is logged in
                roomAvailable ? (
-                 <Link to={`/book/${apartment._id}/${fromDate}/${toDate}`}>
-                   <button className='btn btn-primary'>Book now</button>
-                 </Link>
-               ) : (
-                 <button className='btn btn-primary' disabled>Apartment taken</button>
-               )
-             ) : (
-               <Link to={`/login`}> {/* Redirect to login if not logged in */}
-                 <button className='btn btn-primary'>Login to book</button>
-               </Link>
-             )
-           ) : (
-             <Link to={`/`}>
-               <button className='btn btn-danger' disabled>Select date first!</button>
-             </Link>
-           )}
+                  <Link to={`/bookAP/${apartment._id}/${fromDate}/${toDate}`}>
+                    <button className='btn btn-primary'>Book now</button>
+                  </Link>
+                ) : (
+                  <button className='btn btn-primary' disabled>Room taken</button>
+                )
+              ) : (
+                <Link to={`/`}>
+                  <button className='btn btn-danger' disabled>Select date first!</button>
+                </Link>
+              )}
     </Card.Footer>
   </Card>
   );
